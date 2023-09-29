dda79508a80f8180ea0d87418626d1c43b776d62
lenin <leninriv@gmail.com>
2023-09-28 20:51:26 -0500

    modal fixes

--- screens/GlobalContainer.tsx
+++ screens/GlobalContainer.tsx
@@ -18,19 +18,19 @@ const Stack = createNativeStackNavigator();
 
 export default function GlobalContainer(props: any) {
     const { navigation, route } = props;
-    const { title, colors, buttons } = route.params;
+    const { title, colors, buttons, modal } = route.params;
 
     useEffect(() => {
         // const routes = navigation.getState().routeNames;
-        // console.log('routes', routes);
+        // console.log('routes', route.params);
     }, []);
 
     return (
         <View style={styles.subNavContainer}>
             <NavigationContainer independent={true}>
                 <Stack.Navigator screenOptions={InspectionTabBarOptions} initialRouteName="initial">
-                    {renderInitial(title, buttons, navigation, colors)}
-                    {renderDynamicScreens(navigation, returnLevelScreens(buttons), colors)}
+                    {renderInitial(title, buttons, navigation, colors, modal)}
+                    {renderDynamicScreens(navigation, returnLevelScreens(buttons), colors, modal)}
                     <Stack.Screen name='directory_result' component={SearchContainer} />
                     <Stack.Screen name="DirectoryView" component={DirectoryViewScreen} />
                     <Stack.Screen name='TagFiltering' component={SearchContainer} />
@@ -40,22 +40,23 @@ export default function GlobalContainer(props: any) {
     );
 }
 
-const renderInitial = (title: string, buttons: any[], mainNav: any, colors: string[]) => {
+const renderInitial = (title: string, buttons: any[], mainNav: any, colors: string[], modal: string) => {
     const item = {
         title: title,
         route: 'initial',
         isFirst: true,
-        buttons
+        buttons,
+        modal
     };
-    return < Stack.Screen name={'initial'} component={ButtonListScreen} initialParams={{ item, colors, mainNav }} />
+    return < Stack.Screen name={'initial'} component={ButtonListScreen} initialParams={{ item, colors, mainNav, modal }} />
 }
 
-const renderDynamicScreens = (mainNav: any, screensList: any[], colors: string[]) => {
+const renderDynamicScreens = (mainNav: any, screensList: any[], colors: string[], modal: string) => {
     const routes: string[] = [];
     return screensList.map((item: any, index) => {
         if (routes.includes(item.route)) return;
         routes.push(item.route);
-        return (<Stack.Screen key={index} name={item.route} component={item.screen ? InfoScreenContainer : ButtonListScreen} initialParams={{ item, colors, mainNav }} />)
+        return (<Stack.Screen key={index} name={item.route} component={item.screen ? InfoScreenContainer : ButtonListScreen} initialParams={{ item, colors, mainNav, modal }} />)
     });
 }
 
@@ -75,8 +76,14 @@ function returnLevelScreens(screensList: any[]) {
 
 function ButtonListScreen(props: any) {
     const { navigation, route } = props;
-    const { item, colors, mainNav } = route.params;
+    const { item, colors, mainNav, modal } = route.params;
     const org = item;
+
+    useEffect(() => {
+        console.log('route.name', org.modal);
+        // console.log('route.params', modal);
+    }, []);
+
     if (org.isFirst) props.navigation = mainNav;
     if (org.buttons?.length) org.buttons = org.buttons.filter((v: any, i: any, a: any) => a.findIndex((t: any) => (t.title === v.title)) === i);
     return <MainLayout  {...props} headerTitle={org.title} backButton>
